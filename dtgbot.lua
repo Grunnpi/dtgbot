a038f09511665acf2fdd25bc9b9c1e426226b3c8
Grunnpi <pierre.grunnagel@gmail.com>
2019-06-26 17:14:04 +0200

    version++

--- dtgbot.lua
+++ dtgbot.lua
@@ -20,7 +20,7 @@ mime   = require("mime")
 
 
 -- version
-g_dtgbot_version = 'v0.8.1'
+g_dtgbot_version = 'v0.9.0'
 
 function environmentVariableDomoticz(envvar)
     -- loads get environment variable and prints in log
