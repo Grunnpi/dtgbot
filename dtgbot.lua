187db68094de69533582de48f2fc307f8263d481
Grunnpi <pierre.grunnagel@gmail.com>
2019-07-10 08:51:06 +0200

    ssh rmlogs fix

--- dtgbot.lua
+++ dtgbot.lua
@@ -20,7 +20,7 @@ mime   = require("mime")
 
 
 -- version
-g_dtgbot_version = 'v0.9.13'
+g_dtgbot_version = 'v0.9.14'
 
 function environmentVariableDomoticz(envvar)
     -- loads get environment variable and prints in log
