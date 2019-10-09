15a13b4053fcc58bc038bbec9dcef6302a454608
Grunnpi <pierre.grunnagel@gmail.com>
2019-10-09 12:46:04 +0200

    bp_get.sh fix

--- bash/bp_get.sh
+++ bash/bp_get.sh
@@ -3,8 +3,7 @@
 ReplyTo=$1
 MessageId=$2
 
-curl -s -X POST "https://api.telegram.org/bot"$TelegramBotToken"/sendPhoto" -F chat_id=$ReplyTo -F reply_to_message_id=$MessageId -F photo="@"$TempFileDir"/bp.csv" -F caption="BankPerfect cash"
-rm /var/tmp/toto.jpg
+curl -s -X POST "https://api.telegram.org/bot"$TelegramBotToken"/sendDocument" -F chat_id=$ReplyTo -F reply_to_message_id=$MessageId -F photo="@"$TempFileDir"/bp.csv" -F caption="BankPerfect cash"
 
 
 
--- dtglib_telegram.lua
+++ dtglib_telegram.lua
@@ -12,17 +12,3 @@ function telegram_SendMsg(SendTo, Message, MessageId, pReplyMarkup)
     print_info_to_log(1,'telegram:msg sent status='..status)
     return
 end
-
-function telegram_SendFile(SendTo, Message, MessageId, pReplyMarkup)
-    local response
-    local status
-    if pReplyMarkup == nil or pReplyMarkup == "" then
-        print_info_to_log(1, g_TelegramApiUrl .. 'sendMessage?chat_id=' .. SendTo .. '&reply_to_message_id=' .. MessageId .. '&text=' .. url_encode(Message))
-        response, status = https.request(g_TelegramApiUrl .. 'sendMessage?chat_id=' .. SendTo .. '&reply_to_message_id=' .. MessageId .. '&text=' .. url_encode(Message))
-    else
-        print_info_to_log(1, g_TelegramApiUrl .. 'sendMessage?chat_id=' .. SendTo .. '&reply_to_message_id=' .. MessageId .. '&text=' .. url_encode(Message) .. '&reply_markup=' .. url_encode(pReplyMarkup))
-        response, status = https.request(g_TelegramApiUrl .. 'sendMessage?chat_id=' .. SendTo .. '&reply_to_message_id=' .. MessageId .. '&text=' .. url_encode(Message) .. '&reply_markup=' .. url_encode(pReplyMarkup))
-    end
-    print_info_to_log(1,'telegram:msg sent status='..status)
-    return
-end
