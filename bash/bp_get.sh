ba82c21d57a5ccdafdff968100eea6194706d24c
Grunnpi <pierre.grunnagel@gmail.com>
2019-10-09 15:09:02 +0200

    fix sendDocument api call

--- bash/bp_get.sh
+++ bash/bp_get.sh
@@ -3,8 +3,5 @@
 ReplyTo=$1
 MessageId=$2
 
-curl -s -X POST "https://api.telegram.org/bot"$TelegramBotToken"/sendDocument" -F chat_id=$ReplyTo -F reply_to_message_id=$MessageId -F photo="@"$TempFileDir"/bp.csv" -F caption="BankPerfect cash"
-
-
-
+curl -s -X POST "https://api.telegram.org/bot"$TelegramBotToken"/sendDocument" -F chat_id=$ReplyTo -F reply_to_message_id=$MessageId -F document="@"$TempFileDir"/bp.csv" -F caption="BankPerfect cash"
 
--- dtgbot.lua
+++ dtgbot.lua
@@ -20,7 +20,7 @@ mime   = require("mime")
 
 
 -- version
-g_dtgbot_version = 'm0.9.24'
+g_dtgbot_version = 'm0.9.25'
 
 function environmentVariableDomoticz(envvar)
     -- loads get environment variable and prints in log
