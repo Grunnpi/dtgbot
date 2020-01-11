ffb5fedcbb22ce8edc945dddc4f9e54d70606f6a
Grunnpi <pierre.grunnagel@gmail.com>
2020-01-11 11:27:37 +0100

    tchat mode

--- dtglib_tchat.lua
+++ dtglib_tchat.lua
@@ -9,5 +9,7 @@ end
 
 -- main handle function
 function handleTchat(telegramMsg_ReplyToId, telegramMsg_MsgId, ReceivedText)
+    print_info_to_log(0, 'Tchat mode activated : [' .. ReceivedText .. ']')
+
     telegram_SendMsg(telegramMsg_ReplyToId, "Ok " .. g_currentUserName .. ", j'ai compris que tu as dit : '" .. ReceivedText .. "'", telegramMsg_MsgId)
 end
