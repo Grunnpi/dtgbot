ea4bb6bf1e3ade0f229c484c846d614438a7d2f7
Grunnpi <pierre.grunnagel@gmail.com>
2020-02-02 18:46:30 +0100

    update idx for garage

--- bash/bouton_garage_2.sh
+++ bash/bouton_garage_2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -s "http://"$DomoticzIP":"$DomoticzPort"/json.htm?type=command&param=switchlight&idx=6&switchcmd=On"
+curl -s "http://"$DomoticzIP":"$DomoticzPort"/json.htm?type=command&param=switchlight&idx=5&switchcmd=On"
 
 
