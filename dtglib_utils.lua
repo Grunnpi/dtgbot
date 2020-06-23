c50531d7f47d47b9b04662b17a75cc4c691e17a7
Grunnpi <Mdpgithub1976+1978=4>
2020-06-23 16:29:54 +0200

    match for filter

--- dtglib_utils.lua
+++ dtglib_utils.lua
@@ -67,7 +67,7 @@ function ChkInTableMatch(itab, idev)
     if itab ~= nil then
         for dev in string.gmatch(itab, "[^|,]+") do
             cnt = cnt + 1
-            if string.gmatch(idev,dev) ~= nil then
+            if string.match(idev,dev) then
                 print_info_to_log(3, "-< ChkInTableMatch found: [" .. idev .. '] match(' .. dev .. ')')
                 return true, cnt
             end
